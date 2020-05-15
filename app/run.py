8778efcad7e980e80393e0c5d86ef3182e159493
Pranav <pranav4code@gmail.com>
2020-05-16 04:02:58 +0530

    Added the data folder and a some visualization change

--- app/run.py
+++ app/run.py
@@ -1,16 +1,14 @@
-import nltk
 import json
 import plotly
 import pandas as pd
-from plotly import graph_objs as go
+import numpy as np
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-nltk.download(['punkt','wordnet'])
 
 from flask import Flask
-from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar, Histogram
+from flask import render_template, request
+from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
@@ -40,98 +38,99 @@ model = joblib.load("../models/classifier.pkl")
 @app.route('/')
 @app.route('/index')
 def index():
-    
-    # extract data needed for visuals
-    # Viz 1
-    genre = df.groupby('genre').count()['id'].sort_values()
-    
-    # Viz 2
-    df['text length'] = df['message'].apply(lambda x: len(x.split()))
-    histogram = df[df['text length'] < 100].groupby('text length').count()['id']
 
-    # Viz 3
-    total_category = df.drop(columns=['id','message','original','genre', 'text length']).sum().sort_values(ascending=False).head(5)
+    # extract data needed for visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+
+    # Show distribution of different category
+    category = list(df.columns[4:])
+    category_counts = []
+    for column_name in category:
+        category_counts.append(np.sum(df[column_name]))
+
+    # extract data exclude related
+    categories = df.iloc[:,4:]
+    categories_mean = categories.mean().sort_values(ascending=False)[1:11]
+    categories_names = list(categories_mean.index)
+
 
     # create visuals
     graphs = [
         {
             'data': [
                 Bar(
-                    x=genre.values,
-                    y=genre.index,
-                    orientation='h'
+                    x=genre_names,
+                    y=genre_counts
                 )
             ],
 
             'layout': {
                 'title': 'Distribution of Message Genres',
                 'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
                     'title': "Genre"
-                },
-                'xaxis': {
-                    'title': "Counts"
                 }
             }
         },
-
         {
             'data': [
                 Bar(
-                    x=histogram.index,
-                    y=histogram.values
+                    x=category,
+                    y=category_counts
                 )
             ],
 
             'layout': {
-                'title': 'Distribution of Messages Length',
+                'title': 'Distribution of Message Categories',
                 'yaxis': {
-                    'title': "Total Messages"
-                },
-                'xaxis': {
-                    'title': "Total Words"
-                }
-            }
-        },
-
-        {
-            'data': [
-                Bar(
-                    x=total_category.index,
-                    y=total_category.values
-                )
-            ],
-
-            'layout': {
-                'title': 'Total Messages per Category (Top 5)',
-                'yaxis': {
-                    'title': "Total"
+                    'title': "Count"
                 },
                 'xaxis': {
                     'title': "Category"
                 }
             }
+        },
+        {
+            'data': [
+                Bar(
+                    x=categories_names,
+                    y=categories_mean
+                )
+            ],
+
+            'layout': {
+                'title': 'Top 10 Message Categories',
+                'yaxis': {
+                    'title': "Percentage"
+                },
+                'xaxis': {
+                    'title': "Categories"
+                }
+            }
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
-
 
 # web page that handles user query and displays model results
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
